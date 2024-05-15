2db4279bcd97225641f76bbe29b3469c85d37913
Samuel Price <sjp@Samuels-MacBook-Pro.local>
2024-05-14 23:13:53 -0400

    db

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path  
 # connects the setting for routing
 from .views import projects_view
+
+from django.conf.urls.static import static
+from django.conf import settings
 # connects views and the function
 
 urlpatterns = [
@@ -13,3 +16,4 @@ urlpatterns = [
 
     
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
